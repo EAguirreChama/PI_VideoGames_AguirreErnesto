8f93bfdecf5efceac82659e2b5c6eef2737a5c0f
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:20:04 -0300

    Update getGenresHandler.js

--- api/src/handlers/GenresHandler/getGenresHandler.js
+++ api/src/handlers/GenresHandler/getGenresHandler.js
@@ -1,15 +1,12 @@
 const getGenresController = require("../../controllers/GenresControllers/getGenresController");
 
-
 const getGenresHandler = async (req , res) => {
     try {
         const allGenres = await getGenresController();
-        
         return res.status(200).json(allGenres);
-
     } catch (error) {
         return res.status(400).json({error: error.message})
     }
 };
 
-module.exports = getGenresHandler
+module.exports = getGenresHandler;
