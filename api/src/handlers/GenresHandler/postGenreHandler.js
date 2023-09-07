907d92ce33941ec0a706a0b60b1bd13e9312ca33
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:20:20 -0300

    Update postGenreHandler.js

--- api/src/handlers/GenresHandler/postGenreHandler.js
+++ api/src/handlers/GenresHandler/postGenreHandler.js
@@ -2,16 +2,12 @@ import postGenreController from "../../controllers/GenresControllers/postGenreCo
 
 const postGenreHandler = async (req, res) => {
     try {
-
         const { name } = req.body
-
         const newGenre = await postGenreController(name)
-
         return res.status(404).json(newGenre)
-
     } catch (error) {
         return res.status(404).json({error: error.message})
     }
-}
+};
 
-module.exports = postGenreHandler
+module.exports = postGenreHandler;
