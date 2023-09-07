aa75b70bb5f9ff75bf5132cbcaae2eda7aba5279
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:15:34 -0300

    Update games15Controller.js

--- api/src/controllers/GamesControllers/games15Controller.js
+++ api/src/controllers/GamesControllers/games15Controller.js
@@ -1,13 +1,8 @@
 const gamesName = require ("../../helpers/gamesName")
 
 const games15Controller = async (name) => {
-
     const games = await gamesName(name)
-
-
     if(games.length) return games.slice(0, 15)
-
     else throw Error ("No se encuentra el VideoGame con ese nombre")
 };
-
 module.exports = games15Controller
