f8b9c1472dd000aba712db818139f4b448f1a5c6
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:18:48 -0300

    Update deleteGameHandler.js

--- api/src/handlers/GamesHandler/deleteGameHandler.js
+++ api/src/handlers/GamesHandler/deleteGameHandler.js
@@ -3,14 +3,11 @@ const deleteGameController = require ("../../controllers/GamesControllers/delete
 const deleteGameHandler = async (req , res) => {
     const id = req.params
     try {
-        
         const game = await deleteGameController(id.idVideogame)
-
         return res.status(200).json({Mensaje: game})
-
     } catch (error) {
         return res.status(400).json({error: "Error al eliminar el juego"})
     }
-}
+};
 
-module.exports = deleteGameHandler
+module.exports = deleteGameHandler;
