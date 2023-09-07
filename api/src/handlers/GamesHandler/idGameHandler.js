c80c32b07aed60bd52dfd88ede3115dcf9a2da84
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:19:09 -0300

    Update idGameHandler.js

--- api/src/handlers/GamesHandler/idGameHandler.js
+++ api/src/handlers/GamesHandler/idGameHandler.js
@@ -1,13 +1,11 @@
 const idGameController = require ("../../controllers/GamesControllers/idGameController")
+
 const idGameHandler = async (req , res) => {
     const { idVideogame} = req.params
     try {
-        
         const idGame = await idGameController(idVideogame)
-
         if(idGame) return res.status(200).json(idGame)
         else throw Error (`No existe juego con Id: ${id}`)
-
     } catch (error) {
         return res.status(400).json({error : error.message})
     }
