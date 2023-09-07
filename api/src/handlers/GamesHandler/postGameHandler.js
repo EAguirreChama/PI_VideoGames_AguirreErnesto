36b18690ecac04f5dcc62079eb03286786e70e65
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:19:27 -0300

    Update postGameHandler.js

--- api/src/handlers/GamesHandler/postGameHandler.js
+++ api/src/handlers/GamesHandler/postGameHandler.js
@@ -2,13 +2,9 @@ const postGameController = require ("../../controllers/GamesControllers/postGame
 
 const postGameHandler = async (req , res) => {
     try {
-
         const { name, description, platforms, background_image, released, rating , genres} = req.body
-        
         const newGame = await postGameController(name, description, platforms, background_image, released, rating , genres)
-        
         return res.status(201).json(newGame)
-        
     } catch (error) {
         return res.status(404).json({error: error.message})   
     }
