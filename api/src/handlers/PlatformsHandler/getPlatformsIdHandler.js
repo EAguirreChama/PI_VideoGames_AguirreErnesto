878db35a1ee0998e08e599d916d6c76d62b5b330
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-07 10:20:53 -0300

    Update getPlatformsIdHandler.js

--- api/src/handlers/PlatformsHandler/getPlatformsIdHandler.js
+++ api/src/handlers/PlatformsHandler/getPlatformsIdHandler.js
@@ -1,13 +1,10 @@
 const getPlatFilterController = require("../../controllers/PlatformsController/getPlatformsIdController");
 
-
 const getPlatFilterHandler = async (req , res) => {
     try {
         const { id } = req.params
         const allPlatforms = await getPlatFilterController(id)
-
         return res.status(200).json(allPlatforms)
-
     } catch (error) {
         return res.status(400).json({error: error.message})
     }
