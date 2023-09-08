2579ef3bef7e0443812bf1396bbf950962fe14cf
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-08 16:30:37 -0300

    Update filterGenres.js

--- client/src/redux/ActionsGenres/filterGenres.js
+++ client/src/redux/ActionsGenres/filterGenres.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const GENRES_FILTER = "GENRES_FILTER"
-
 export const genresFilter = (id) => {
     return async function (dispatch) {
         const apiData = await axios.get(`http://localhost:3001/genres/${id}`)
