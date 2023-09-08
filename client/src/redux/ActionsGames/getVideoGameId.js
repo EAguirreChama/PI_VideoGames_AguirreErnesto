95381b6c1d39a8a9c52a8ed36d8de1a4d9dc13b8
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-08 16:29:29 -0300

    Update getVideoGameId.js

--- client/src/redux/ActionsGames/getVideoGameId.js
+++ client/src/redux/ActionsGames/getVideoGameId.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
 export const GET_VIDEOGAME_ID = "GET_VIDEOGAME_ID"
-
 export const getVideoGameId = (id) => {
     return async function (dispatch) {
     const result = await axios.get(`http://localhost:3001/videogames/${id}`)
