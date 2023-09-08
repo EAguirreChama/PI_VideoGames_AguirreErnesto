84afbdbef9090de7be0489cea5ac7067f5838bfd
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-08 16:29:44 -0300

    Update getVideoGameName.js

--- client/src/redux/ActionsGames/getVideoGameName.js
+++ client/src/redux/ActionsGames/getVideoGameName.js
@@ -1,13 +1,10 @@
 import axios from "axios"
 
 export const GET_VIDEOGAMES_NAME = "GET_VIDEOGAMES_NAME"
-
 export const getVideoGameName = (name) => {
-
     return async function (dispatch) {
         const result = await axios.get(`http://localhost:3001/videogames?name=${name}`)
         const videoGameName = result.data;
-
-            dispatch({ type: GET_VIDEOGAMES_NAME , payload: videoGameName})
+         dispatch({ type: GET_VIDEOGAMES_NAME , payload: videoGameName})
     };
 };
