10d50e59bb0647ae21459bec3d9f21cf3c8dfdfd
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-08 16:29:53 -0300

    Update getVideoGames.js

--- client/src/redux/ActionsGames/getVideoGames.js
+++ client/src/redux/ActionsGames/getVideoGames.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
 export const GET_VIDEOGAMES = "GET_VIDEOGAMES"
-
 export const getVideoGames = () => {
     return async function (dispatch) {
     const apiData = await axios.get ("http://localhost:3001/videogames")
