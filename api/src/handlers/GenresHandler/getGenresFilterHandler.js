f238fa89085e36d5c4abb2ae4862508821c100ae
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-14 11:56:56 -0300

    Update getGenresFilterHandler.js

--- api/src/handlers/GenresHandler/getGenresFilterHandler.js
+++ api/src/handlers/GenresHandler/getGenresFilterHandler.js
@@ -3,7 +3,9 @@ const byGenreFiltered = require("../../controllers/GenresControllers/byGenreFilt
 const byGenreFilteredHandler = async (req , res) => {
     try {
         const { id } = req.params
+        
         const allGenres = await byGenreFiltered(id)
+        
         return res.status(200).json(allGenres)
     } catch (error) {
         return res.status(400).json({error: error.message})
