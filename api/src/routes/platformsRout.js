6d61ea4c1395a1bcb3cb27c2099f5a60f5dc07f3
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-14 12:00:14 -0300

    Update platformsRout.js

--- api/src/routes/platformsRout.js
+++ api/src/routes/platformsRout.js
@@ -1,11 +1,13 @@
 const { Router } = require ("express");
-const getPlatformsHandler = require ("../handlers/PlatformsHandler/getPlatformsHandler");
-const getGenresFilterHandler = require ("../handlers/PlatformsHandler/getPlatformsIdHandler")
 
 const PlatformsRout = Router();
 
+const getPlatformsHandler = require ("../handlers/PlatformsHandler/getPlatformsHandler");
+const getGenresFilterHandler = require ("../handlers/PlatformsHandler/getPlatformsIdHandler")
+
 // Ruta para obtener todos las plataformas
 PlatformsRout.get("/", getPlatformsHandler);
+
 // Ruta para obtener una plataforma
 PlatformsRout.get("/:id", getGenresFilterHandler);
 
