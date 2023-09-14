36502866ba87df5dd1cdbfa3420a83a319f2a2e8
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-14 12:00:47 -0300

    Update videoGamesRout.js

--- api/src/routes/videoGamesRout.js
+++ api/src/routes/videoGamesRout.js
@@ -1,20 +1,23 @@
 const { Router } = require ("express");
 
+const VideoGamesRout = Router();
+
 const allGamesHandler = require ("../handlers/GamesHandler/allGamesHandler");
 const idGameHandler = require ("../handlers/GamesHandler/idGameHandler");
 const postGameHandler = require ("../handlers/GamesHandler/postGameHandler");
 const deleteGameHandler = require ("../handlers/GamesHandler/deleteGameHandler")
 
-const VideoGamesRout = Router();
-
 // Rutas
 
 // Ruta para traer todos los VideoGames con su informacion y buscar por nombre
 VideoGamesRout.get("/" , allGamesHandler);
+
 // Ruta para traer un VideoGame por su Id
 VideoGamesRout.get("/:idVideogame" , idGameHandler);
+
 // Ruta para crear un VideoGame con los datos necesarios
 VideoGamesRout.post("/" , postGameHandler);
+
 // Ruta para eliminar un VideoGame por su Id de la Base de Datos
 VideoGamesRout.delete("/:idVideogame" , deleteGameHandler)
 
