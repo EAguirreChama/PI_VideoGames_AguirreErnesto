ba0cc7445e912954095e535c85f8a9f97284dab1
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-20 11:13:58 -0300

    Update getGenres.js

--- client/src/redux/ActionsGenres/getGenres.js
+++ client/src/redux/ActionsGenres/getGenres.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 
 export const GET_GENRES = "GET_GENRES"
+
 export const getGenres = () => {
     return async function (dispatch) {
         const apiData = await axios.get("http://localhost:3001/genres")
-        const genres = apiData.data;
+        const genres = apiData.data
             dispatch({ type: GET_GENRES , payload: genres})
-    };
-};
+    }
+}
