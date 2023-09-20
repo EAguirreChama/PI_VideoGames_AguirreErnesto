f5233de326425e75399e562bec34796639373d6d
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-20 11:11:23 -0300

    Update filterRatingGames.js

--- client/src/redux/ActionsGames/filterRatingGames.js
+++ client/src/redux/ActionsGames/filterRatingGames.js
@@ -1,7 +1,8 @@
 export const RATING_VIDEOGAMES = "RATING_VIDEOGAMES"
+
 export const ratingVideoGames = (rating) => {
     return {
         type: RATING_VIDEOGAMES,
         payload: rating
-    };
-};
+    }
+}
