6751918c413e1e6d7c85f26371706f7f2ea86197
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-20 11:14:14 -0300

    Update filterPlatforms.js

--- client/src/redux/ActionsPlatforms/filterPlatforms.js
+++ client/src/redux/ActionsPlatforms/filterPlatforms.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from "axios"
 
 export const PLATFORMS_FILTER = "PLATFORMS_FILTER"
 
 export const platformsFilter = (id) => {
     return async function (dispatch) {
         const apiData = await axios.get(`http://localhost:3001/platforms/${id}`)
-        const filter = apiData.data;
+        const filter = apiData.data
         dispatch({ type: PLATFORMS_FILTER, payload: filter})
-    };
-};
+    }
+}
