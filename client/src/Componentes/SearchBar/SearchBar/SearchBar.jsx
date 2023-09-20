a725ce4a53c0f43c0dc49fd8521652a3ce914505
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-19 21:41:28 -0300

    Update SearchBar.jsx

--- client/src/Componentes/SearchBar/SearchBar/SearchBar.jsx
+++ client/src/Componentes/SearchBar/SearchBar/SearchBar.jsx
@@ -1,9 +1,9 @@
-import OrderAlf from "../FilterGames/FilterGames";
-import FilterGenres from "../FilterGenres/FilterGenres";
-import OriginGame from "../FilterOrigin/FilterOrigin";
-import Rating from "../FilterRating/FilterRating";
-import FilterPlatforms from "../FilterPlatforms/FilterPlatforms";
-import Search from "./Search";
+import OrderAlf from "../FilterGames/FilterGames"
+import FilterGenres from "../FilterGenres/FilterGenres"
+import OriginGame from "../FilterOrigin/FilterOrigin"
+import Rating from "../FilterRating/FilterRating"
+import FilterPlatforms from "../FilterPlatforms/FilterPlatforms"
+import Search from "./Search"
 import style from "./SearchBar.module.css"
 
 const SearchBar = () => {
@@ -19,5 +19,6 @@ const SearchBar = () => {
             <Search/>
         </div>
     )
-}:
-export default SearchBar;
+}
+
+export default SearchBar
